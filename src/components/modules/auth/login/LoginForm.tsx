f1865f98e0c873c1f963367b3e9b8a9005363142
Md Hasanuzzaman Shohag <hzaman.live@gmail.com>
2025-10-24 23:06:40 +0600

    login page added

---
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginForm = () => {
+    return (
+        <div>
+            <h2>This is the Login Page</h2>
+        </div>
+    );
+};
+
+export default LoginForm;
