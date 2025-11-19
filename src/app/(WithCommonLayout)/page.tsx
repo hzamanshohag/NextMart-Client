8855857e0299af983a01e290deac284b46bc6773
Md Hasanuzzaman Shohag <hzaman.live@gmail.com>
2025-11-19 19:09:55 +0600

    dedecode-current-user

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,11 +1,14 @@
+import { getCurrentUser } from "@/services/AuthService";
 
+const HomePage = async () => {
+  const user = await getCurrentUser();
+  console.log(user);
 
-const HomePage= () => {
-    return (
-        <div>
-            <h1>Welcome to NextMart Home Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to NextMart Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,4 +1,5 @@
 "use server";
+import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
 
@@ -13,9 +14,9 @@ export const registerUser = async (userData: FieldValues) => {
       body: JSON.stringify(userData),
     });
     const result = await res.json();
-    const storeCookie = await cookies();
+    const storeCookies = await cookies();
     if (result.success && result.data?.accessToken) {
-      storeCookie.set("accessToken", result.data.accessToken);
+      storeCookies.set("accessToken", result.data.accessToken);
     }
     return result;
   } catch (error: any) {
@@ -24,6 +25,7 @@ export const registerUser = async (userData: FieldValues) => {
 };
 
 export const loginUser = async (userData: FieldValues) => {
+  // Implement get login user logic here
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/login`, {
       method: "POST",
@@ -34,13 +36,26 @@ export const loginUser = async (userData: FieldValues) => {
     });
 
     const result = await res.json();
-    const storeCookie = await cookies();
+    const storeCookies = await cookies();
     if (result.success && result.data?.accessToken) {
-      storeCookie.set("accessToken", result.data.accessToken);
+      storeCookies.set("accessToken", result.data.accessToken);
     }
 
     return result;
   } catch (error: any) {
     return Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  // Implement get current user logic here
+  const storeCookies = await cookies();
+  const accessToken = storeCookies.get("accessToken")?.value;
+  let decodedUser = null;
+  if (accessToken) {
+    decodedUser = await jwtDecode(accessToken);
+    return decodedUser;
+  } else {
+    return null;
   }
 };
